43a4e2b7262b31cbc58778a7c48968a977bc034a
anfusion <nzmoriarty@gmail.com>
2020-06-10 12:21:02 +1200

    update build of picsearch

---
+++ picture_search/precache-manifest.e2ec7e2900bcae9357da94a9780538f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddab66cc71a4e7afa7a99ea9dba8ad7e",
+    "url": "/picture_search/index.html"
+  },
+  {
+    "revision": "927ac8b88e547b5e56fd",
+    "url": "/picture_search/static/css/main.008e8a6d.chunk.css"
+  },
+  {
+    "revision": "71752e1f254d7fdddf9d",
+    "url": "/picture_search/static/js/2.3f040f9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/picture_search/static/js/2.3f040f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "927ac8b88e547b5e56fd",
+    "url": "/picture_search/static/js/main.0560c253.chunk.js"
+  },
+  {
+    "revision": "e9575904f5826ed628de",
+    "url": "/picture_search/static/js/runtime-main.a9e945ac.js"
+  }
+]);
--- picture_search/service-worker.js
+++ picture_search/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b8385b48c26fe3bcf028c97ccee585f.js"
+  "/picture_search/precache-manifest.e2ec7e2900bcae9357da94a9780538f7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/picture_search/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
