8e37b9b9643f66dcdfe767e03b60a3a72966efba
anfusion <nzmoriarty@gmail.com>
2020-06-10 12:10:05 +1200

    added picture search app

---
+++ picture_search/precache-manifest.8b8385b48c26fe3bcf028c97ccee585f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833486c00c600380d1e5f135c96cbb8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "927ac8b88e547b5e56fd",
+    "url": "/static/css/main.008e8a6d.chunk.css"
+  },
+  {
+    "revision": "71752e1f254d7fdddf9d",
+    "url": "/static/js/2.3f040f9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3f040f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "927ac8b88e547b5e56fd",
+    "url": "/static/js/main.0560c253.chunk.js"
+  },
+  {
+    "revision": "46525b4c7b15e71e68ae",
+    "url": "/static/js/runtime-main.5fd334c5.js"
+  }
+]);
