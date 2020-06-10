c1e772ddc1eca03c88de399f6f0380cbf8e15330
anfusion <nzmoriarty@gmail.com>
2020-06-10 15:02:36 +1200

    nicky tube readd

---
+++ nicky_tube/precache-manifest.905de16ccfbc81ea24f98efe50a7af4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1bee08182667bc2dd1d3f8cc4bb13e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cb0ccbe72ab8e45efef",
+    "url": "/static/css/main.b56be4e3.chunk.css"
+  },
+  {
+    "revision": "2b21845523176c526f4c",
+    "url": "/static/js/2.03133b37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.03133b37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb0ccbe72ab8e45efef",
+    "url": "/static/js/main.787e0493.chunk.js"
+  },
+  {
+    "revision": "7d499a4ae5ba60415a49",
+    "url": "/static/js/runtime-main.b2354d74.js"
+  }
+]);
--- nicky_tube/service-worker.js
+++ nicky_tube/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1a8e00014ef07ef148367fdb9c91cac5.js"
+  "/precache-manifest.905de16ccfbc81ea24f98efe50a7af4c.js"
 );
 
 self.addEventListener('message', (event) => {
